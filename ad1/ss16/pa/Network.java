f38b265a7d3ade2fce4b873937b02be12c26b766
Martin Weise <e1429167@student.tuwien.ac.at>
2016-05-04 13:20:09 +0200

    improved worst-case time complexity at method numberOfConnections()

--- ad1/ss16/pa/Network.java
+++ ad1/ss16/pa/Network.java
@@ -5,16 +5,18 @@ import java.util.*;
 public class Network {
 
     private HashMap<Integer, LinkedList<Integer>> A = new HashMap<>();
-    private int n;
+    private final int n;
+    private int m;
     private boolean[][] M;
     private boolean[] discovered;
 
     /**
-     * O(n)
+     * O(n^2)
      * Organisation in Form einer Adjaszenzliste
      */
     public Network(int n) {
         this.n = n;
+        this.m = 0;
         this.M = new boolean[n][n];
         for(int i = 0; i < n; i++) {
             this.A.put(i, new LinkedList<>());
@@ -30,15 +32,11 @@ public class Network {
     }
 
     /**
-     * O(n)
+     * O(1)
      * Liefert die Anzahl der Verbindungen zurück.
      */
     public int numberOfConnections() {
-        int c = 0;
-        for(LinkedList node : this.A.values()) {
-            c += node.size();
-        }
-        return c;
+        return m;
     }
 
     /**
@@ -47,6 +45,7 @@ public class Network {
      * vorhanden,dann passiert nichts, d.h. die Verbindung bleibt im Netzwerk erhalten.
      */
     public void addConnection(int v, int w) {
+        this.m++;
         this.A.get(v).add(w);
         this.M[v][w] = true;
         this.M[w][v] = true;
@@ -58,37 +57,48 @@ public class Network {
      */
     public void addAllConnections(int v) {
         LinkedList<Integer> node = this.A.get(v);
+        int c = 0;
         for(int k : this.A.keySet()) {
             if(k == v || node.contains(k)) {
                 return;
             }
+            c++;
             node.add(k);
         }
+        this.m += c;
     }
 
     /**
+     * O(n)
      * Entfernt eine Verbindung zwischen den Knoten v und w aus dem Netzwerk. Ist die Verbindung nicht
      * vorhanden, dann passiert nichts.
      */
     public void deleteConnection(int v, int w) {
         if(this.A.containsKey(v) && this.A.containsKey(w)) {
+            this.m--;
             this.A.get(v).remove(w);
+            this.M[v][w] = false;
+            this.M[w][v] = false;
         }
     }
 
     /**
+     * O(n)
      * Entfernt alle Verbindungen für einen bestimmten Knoten v aus dem Netzwerk. Hatte der Knoten noch
      * keine Verbindungen, dann passiert nichts.
      */
     public void deleteAllConnections(int v) {
         if(this.A.containsKey(v)) {
+            this.m -= this.A.get(v).size();
             this.A.get(v).clear();
             this.M[v] = new boolean[this.n];
         }
     }
 
     /**
+     * O(n+m)
      * Liefert die Anzahl der Zusammenhangskomponenten im Netzwerk zurück.
+     * Verwendet: DFS
      */
     public int numberOfComponents() {
         this.discovered = new boolean[this.n];
@@ -96,33 +106,40 @@ public class Network {
         for(int u : this.A.keySet()) {
             if(!discovered[u]) {
                 c++;
-                DFSnum(u);
+                numberOfComponentsR(u);
             }
         }
         return c;
     }
 
-    private void DFSnum(int u) {
+    private void numberOfComponentsR(int u) {
         this.discovered[u] = true;
         for(int v : this.A.get(u)) {
             if(!discovered[v]) {
-                DFSnum(v);
+                numberOfComponentsR(v);
             }
         }
     }
 
     /**
+     * O(n+m)
      * Überprüft, ob das Netzwerk einen Kreis enthält. Wenn dies der Fall ist, wird true zurückgeliefert,
      * ansonsten false.
+     * Verwendet: BFS
      */
     public boolean hasCycle() {
-        int u = 0;
-        for(LinkedList<Integer> L : this.A.values()) {
-            for(int w : L) {
-                for(int v : this.A.keySet()) {
-                    if(M[u][v] && M[w][v]) {
-                        return true;
-                    }
+        boolean[] discovered = new boolean[this.n];
+        discovered[0] = true;
+        LinkedList<Integer> Q = new LinkedList<>();
+        Q.add(0);
+        while(!Q.isEmpty()) {
+            int u = Q.pop();
+            for(int v : this.A.get(u)) {
+                if(!discovered[v]) {
+                    discovered[v] = true;
+                    Q.add(v);
+                } else {
+                    return true;
                 }
             }
         }
@@ -134,42 +151,15 @@ public class Network {
      * start zu einem Endknoten end zu gelangen. Sind start und end gleich, dann soll 0 zurückgeliefert
      * werden. Sind start und end nicht u ̈ber einen Pfad miteinander verbunden, dann wird -1 zurück-
      * geliefert.
+     * Verwendet: Dijkstra's Algorithm
      */
     public int minimalNumberOfConnections(int start, int end) {
         if(start == end) {
             return 0;
         }
-        boolean[] discovered = new boolean[this.n];
-        discovered[start] = true;
-        LinkedList<Integer> Q = new LinkedList<>();
-        Q.add(start);
-        int level = 1;
-        int c = 0;
-        boolean up = true;
-        // TODO: working now, do the same with linked list (undirected, now is directed)
-        while(!Q.isEmpty()) {
-            int u = Q.pop();
-            for(int v = 0; v < this.M[u].length; v++) {
-                if(this.M[u][v] && !discovered[v]) {
-                    if(up)
-                        c++;
-                    else
-                        c--;
-                    if(v == end) {
-                        return level;
-                    }
-                    discovered[v] = true;
-                    Q.add(v);
-                }
-            }
-            if(c > 0) {
-                up = false;
-                level++;
-                c--;
-            } else if(level != 0) {
-                up = true;
-            }
-        }
+        int c = this.numberOfConnections();
+        int[] distance = new int[c];
+        int[] previous = new int[c];
         return -1;
     }
 
@@ -241,39 +231,38 @@ public class Network {
     }
 
     public static void main(String[] args) {
-//        Network network = new Network(13);
-//        network.addConnection(0,1);
-//        network.addConnection(0,2);
-//        network.addConnection(0,6);
-//        network.addConnection(1,3);
-//        network.addConnection(1,4);
-//        network.addConnection(2,4);
-//        network.addConnection(2,6);
-//        network.addConnection(2,7);
-//        network.addConnection(3,4);
-//        network.addConnection(4,5);
-//        network.addConnection(6,7);
-//        network.addConnection(8,9);
-//        network.addConnection(8,10);
-//        network.addConnection(9,10);
-//        network.addConnection(10,11);
-//        network.addConnection(11,12);
-
-        Network network = new Network(7);
+        Network network = new Network(13);
         network.addConnection(0,1);
+        network.addConnection(0,2);
+        network.addConnection(0,6);
         network.addConnection(1,2);
-        network.addConnection(2,3);
+        network.addConnection(1,3);
+        network.addConnection(1,4);
+        network.addConnection(2,4);
+        network.addConnection(2,6);
+        network.addConnection(2,7);
         network.addConnection(3,4);
         network.addConnection(4,5);
-        network.addConnection(5,6);
+        network.addConnection(6,7);
+        network.addConnection(8,9);
+        network.addConnection(8,10);
+        network.addConnection(9,10);
+        network.addConnection(10,11);
+        network.addConnection(11,12);
+
+//        Network network = new Network(7);
+//        network.addConnection(0,1);
+//        network.addConnection(1,2);
+//        network.addConnection(2,3);
+//        network.addConnection(3,4);
+//        network.addConnection(4,5);
+//        network.addConnection(5,6);
 
         System.out.println("numberOfNodes(): " + network.numberOfNodes());
         System.out.println("numberOfConnections(): " + network.numberOfConnections());
         System.out.println("numberOfComponents(): " + network.numberOfComponents());
         System.out.println("hasCycle(): " + network.hasCycle());
-        System.out.println("minimalNumberOfConnections(0,5): " + network.minimalNumberOfConnections(0,5));
-        System.out.println("minimalNumberOfConnections(3,6): " + network.minimalNumberOfConnections(3,6));
-        System.out.println("minimalNumberOfConnections(0,6): " + network.minimalNumberOfConnections(0,6));
+        System.out.println("minimalNumberOfConnections(0,7): " + network.minimalNumberOfConnections(0,7));
 //        System.out.println(network.minimalNumberOfConnections(0,9));
 
     }
